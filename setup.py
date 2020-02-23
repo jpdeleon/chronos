3f2c1efd2295c9c83c5fe792a5ce0d1a6cca19cc
jpdeleon <jpdeleon.bsap@gmail.com>
2020-02-23 15:43:08 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,46 @@
 from setuptools import setup, find_packages
 
+import os
+import subprocess
+import sys
+
+try:
+    import git
+except ModuleNotFoundError:
+    subprocess.call([sys.executable, "-m", "pip", "install", "gitpython"])
+    import git
+
+
+def install_requires():
+    reqdir = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(reqdir, "requirements.txt"), encoding="utf-8") as f:
+        all_packages = f.readlines()
+        packages = [
+            package for package in all_packages if "git+ssh" not in package
+        ]
+        manual_pip_packages = [
+            package for package in all_packages if "git+ssh" in package
+        ]
+        for package in manual_pip_packages:
+            subprocess.call([sys.executable, "-m", "pip", "install", package])
+    return packages
+
+
+def pull_first():
+    """This script is in a git directory that can be pulled."""
+    cwd = os.getcwd()
+    gitdir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(gitdir)
+    g = git.cmd.Git(gitdir)
+    try:
+        g.execute(["git", "lfs", "pull"])
+    except git.exc.GitCommandError:
+        raise RuntimeError("Make sure git-lfs is installed!")
+    os.chdir(cwd)
+
+
+pull_first()
+
 setup(
     name="chronos",
     version="0.0.1",
@@ -17,17 +58,5 @@ setup(
         "scripts/make_cdips_ql",
     ],
     zip_safe=False,
-    install_requires=[
-        "astroquery",
-        "lightkurve",
-        "astropy",
-        "pandas",
-        "tqdm",
-        "astroplan",
-        "transitleastsquares",
-        # "wotan",
-        "scikit-image",
-        "git-lfs",
-        "deepdish",
-    ],
+    install_requires=install_requires(),
 )
